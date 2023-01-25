02612cbefb114540e85ee6fe417b1ecf83da7f52
amanda ranero <amandaranero@gmail.com>
2023-01-25 18:41:40 -0500

    commit tree 6e11811d25315a74a81aea7fcbe10ce4d34fe876

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    Amanda: "67 6th Ave",
+    Paris: "45 4th Ave",
+    Jess: "56 7th Ave",
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    let newEmployee = {...obj}
+    newEmployee[key] = value
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    employee[key]=value
+    return employee
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    let newEmployee = {...obj};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
+
